39ed8e9cf6acc8a5f8f4dee7d378fb096d9e0077
youcefKNL <khenichil_youcef@hotmail.fr>
2023-06-26 22:12:56 +0200

    test

--- backend/routes/contact.routes.js
+++ backend/routes/contact.routes.js
@@ -4,6 +4,7 @@ const nodemailer = require("nodemailer");
 const router = express.Router();
 
 // Configurer Nodemailer
+
 const transporter = nodemailer.createTransport({
   host: "smtp-mail.outlook.com",
   secureConnection: true,
@@ -13,8 +14,8 @@ const transporter = nodemailer.createTransport({
     ciphers: "SSLv3",
   },
   auth: {
-    user: process.env.MAIL,
-    pass: process.env.MAIL_PASS,
+    user: process.env.MAIL, // Hotmail email
+    pass: process.env.MAIL_PASS, // Hotmail password
   },
 });
 
@@ -26,7 +27,7 @@ router.post("/", (req, res) => {
     to: process.env.MAIL, // Adresse e-mail de destination
     subject: `[PORTFOLIO] Nouveau message de ${name}`,
     text: `
-      Nom: ${name} 
+      Nom: ${name}
       Email: ${email}
       Message: ${message}
     `,
@@ -58,3 +59,81 @@ router.post("/", (req, res) => {
 });
 
 module.exports = router;
+
+// const express = require("express");
+// const mailgun = require("mailgun-js");
+
+// const router = express.Router();
+// const mg = mailgun({
+//   apiKey: "ba676f27cefc222805b26fd74e804359-135a8d32-24303809",
+//   domain: "sandbox54b5291e7e82432184083ce2c31d7d81.mailgun.org",
+// });
+
+// router.post("/", (req, res) => {
+//   const { name, email, message } = req.body;
+
+//   const mailData = {
+//     from: "mailgun@portfolio.fr",
+//     to: "khenichil_youcef@hotmail.fr",
+//     subject: `[PORTFOLIO] Nouveau message de ${name}`,
+//     text: `
+//       Nom: ${name}
+//       Email: ${email}
+//       Message: ${message}
+//     `,
+//   };
+
+//   mg.messages().send(mailData, (error, body) => {
+//     if (error) {
+//       console.error(error);
+//       res.status(500).json({
+//         success: false,
+//         error: "Une erreur s'est produite lors de l'envoi de l'e-mail.",
+//       });
+//     } else {
+//       console.log("E-mail envoyé:", body);
+//       res
+//         .status(200)
+//         .json({ success: true, message: "L'e-mail a été envoyé avec succès." });
+//     }
+//   });
+// });
+
+// module.exports = router;
+
+// const express = require("express");
+// const router = express.Router();
+
+// const nodemailer = require("nodemailer");
+// const nodemailerMailgun = require("nodemailer-mailgun-transport");
+
+// const auth = {
+//   auth: {
+//     api_key: "ba676f27cefc222805b26fd74e804359-135a8d32-24303809",
+//     domain: "sandbox54b5291e7e82432184083ce2c31d7d81.mailgun.org",
+//   },
+// };
+
+// let transporter = nodemailer.createTransport(nodemailerMailgun(auth));
+
+// router.post("/", (req, res) => {
+//   const { name, email, message } = req.body;
+
+//   const mailOption = {
+//     from: "Excited User <mailgun@sandbox-123.mailgun.org>",
+//     to: ["khenichil_youcef@hotmail.fr"],
+//     subject: ` [PORTFOLIO] Nouveau message de ${name}`,
+//     text: ` Nom: ${name}       Email: ${email}       Message: ${message}  `,
+//     html: "<h1>Testing some Mailgun awesomeness!</h1>",
+//   };
+
+//   transporter.sendMail(mailOption, function (error, data) {
+//     if (error) {
+//       res.status(500).json({ message: "Erreur : " + error });
+//     } else {
+//       res.status(200).json({ message: "message envoyé !!" + data });
+//     }
+//   });
+// });
+
+// module.exports = router;
